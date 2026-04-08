737b6e24b368a22847aeb8676b833bdaf2a40f32
Shanu Priya <priyads12@gmail.com>
2026-04-07 23:09:30 -0400

    removed foreach to iterate the prodctlist and used array map to iterate and return the promise, to wait before fetching the sales report

--- index.ts
+++ index.ts
@@ -1,16 +1,28 @@
-import { fetchProductCatalog, fetchProductReviews , type Product } from "./apiSimulator.js";
+import { fetchProductCatalog, fetchProductReviews , fetchSalesReport, type Product } from "./apiSimulator.js";
 
 function handleApi(){
     fetchProductCatalog().then((productList)=>{
         console.log(productList);
-        //iterate the product list 
-        productList.forEach((product:Product) =>{
-            fetchProductReviews(product.id).then((result)=>{
+        //iterate the product list using map 
+        const reviewPromises = productList.map((product:Product) =>{
+            return fetchProductReviews(product.id).then((result)=>{
                 console.log(`Reviews of Product: ${product.name}`);
                 console.log(result);
-            })
-        })
-    });
+                return result;
+            });
+        });// this block of code initates the request to fetch the review of the each product and returns the array of promisses for each request
+        return Promise.all(reviewPromises); // add the reviewpromises to wait until all the reviews or either resolved or rejected
+    })
+    .then((allReviews) =>{
+        console.log("All reviews completed.");
+       // console.log(allReviews);
+        //now fetch the final report
+        return fetchSalesReport();
+    })
+    .then((report)=>{
+        console.log("Sales Report");
+        console.log(report);
+    })
 }
 
 handleApi();
